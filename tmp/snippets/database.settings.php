66d440e25a7ced885b142fdf3986037e21f291c3
Patrick Connolly <patrick.c.connolly@gmail.com>
2012-11-17 21:02:32 -0500

    Ugh. Snippet typo.

--- tmp/snippets/database.settings.php
+++ tmp/snippets/database.settings.php
@@ -1,17 +1,17 @@
 preg_match('|^mysql://(.*):(.*)@(.*)/(.*)\?reconnect=true$|', $_ENV['CLEARDB_DATABASE_URL'], $db_url_parts);
 
 $databases = array (
-  'default' =>. 
+  'default' =>
   array (
-    'default' =>. 
+    'default' =>
     array (
       'database' => $db_url_parts[4],
       'username' => $db_url_parts[1],
       'password' => $db_url_parts[2],
       'host' => $db_url_parts[3],
-      'port' => '', 
+      'port' => '',
       'driver' => 'mysql',
-      'prefix' => '', 
-    ),  
-  ),  
+      'prefix' => '',
+    ),
+  ),
 );
