4b2f629b5bc2343a32456724712e47bbe43c9fa3
Patrick Connolly <patrick.c.connolly@gmail.com>
2012-11-17 21:54:05 -0500

    blah

--- tmp/scripts/drop-all-tables.php
+++ tmp/scripts/drop-all-tables.php
@@ -1,11 +1,8 @@
 <?php
 
-require_once './includes/bootstrap.inc';
-drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
-
 # Loop over all tables and drop
 $result = db_query("SHOW TABLES");
 while($row = db_fetch_array($result)) {
   $table = array_pop($row);
-  db_query("DROP $table");
-}
+  $result2 = db_query("DROP $table");
+};
