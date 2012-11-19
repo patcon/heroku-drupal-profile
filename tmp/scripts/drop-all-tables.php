2ade08c7b7ef43b21e0fcad789f0ccc87dfc8aa3
Patrick Connolly <patrick.c.connolly@gmail.com>
2012-11-18 22:01:33 -0500

    Remove drop-all-tables scripts.

--- tmp/scripts/drop-all-tables.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-# Loop over all tables and drop
-$result = db_query("SHOW TABLES");
-while($row = db_fetch_array($result)) {
-  $table = array_pop($row);
-  $result2 = db_query("DROP $table");
-};
